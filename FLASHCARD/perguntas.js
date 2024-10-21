ae044f3f430248cd89078802a2992c716e8b5c3c
gabyteixeira <gabriellyteixeira90@gmail.com>
2024-10-21 10:53:37 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'hobbie favorito',
+    'pintar as unhas'
+)
+
+criaCartao(
+    'pessoal',
+    'esporte favorito?',
+    'vôlei'
+)
+
+criaCartao(
+    'recomendação',
+    'melhor 2º ano do cervantes?',
+    '2º A'
+)
+
+criaCartao(
+    'recomendacão',
+    'fruta favorita?',
+    'melancia'
+)
